86dc55a2d77153db2f2763d4edcc37d902a2aaa7
belovetech <belovetech@gmail.com>
2023-01-05 19:46:15 +0100

    [Feat]: add two integer

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def add(a:float, b:float) -> float:
+    """Add two floats
+    
+    Args:
+        a (float): first integer
+        b (float): second integer
+    
+    Return:
+        sum(float) 
+    """
+    return a + b
