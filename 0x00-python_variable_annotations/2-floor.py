52e29157f7df64c0faba5214f503167fadd0c2d0
belovetech <belovetech@gmail.com>
2023-01-05 20:33:53 +0100

    [Feat]: floor of a float number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function floor"""
+
+
+def floor(n: float) -> int:
+    """Floor of a float number
+
+    Args:
+        n (float): Float integer to get its floor
+
+    Return:
+        (int): floor of the float
+    """
+    return int(n)
