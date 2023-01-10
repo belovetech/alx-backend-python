65e74c1dd5f55615c8f6441ca2c53aaa8d0a69e0
belovetech <belovetech@gmail.com>
2023-01-10 19:36:35 +0100

    [Feat]: generate 10 random number using async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import List
 
 
-async def async_generator() -> None:
+async def async_generator() -> List[float]:
     """Loop couroutines 10 times and yield random number between 0-10"""
     for i in range(10):
         await asyncio.sleep(1)
