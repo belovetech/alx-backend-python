b0e053a86d5694585fc9bbb81ec2c94fb9a83650
belovetech <belovetech@gmail.com>
2023-01-29 18:22:35 +0100

    [Feat]: Test public_repos_url return the coreect valued based on the given payload

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@
 from client import GithubOrgClient
 from parameterized import parameterized
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -22,3 +22,15 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class = GithubOrgClient(org_name)
         test_class.org()
         mock.called_with_once(test_class.ORG_URL.format(org=org_name))
+
+    def test_public_repos_url(self):
+        """Test TestGithubOrgClient.public_repos_url
+        return the correct value based on the given payload
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "something"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
